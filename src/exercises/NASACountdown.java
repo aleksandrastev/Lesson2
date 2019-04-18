c33334d25a95e3246cf9fa46028be42570cf96e4
aleksandrastev <leafsoul91@gmail.com>
2019-04-18 20:37:59 +0200

    First exercise

--- src/exercises/NASACountdown.java
+++ src/exercises/NASACountdown.java
@@ -3,9 +3,22 @@ package exercises;
 /*
  * Count down a rocket launch!
  * 
- * Print the numbers from 10 to 1, then print “blastoff”. 
+ * Print the numbers from 10 to 1, then print blastoff". 
  */
 
 public class NASACountdown {
+	
+	private void countDown()
+	{
+		for(int i=10; i>0; i--)
+		{
+			System.out.println(i);
+		}
+		System.out.println("blastoff");
+	}
+	
+	public static void main(String[] args) {
+		new NASACountdown().countDown();
+	}
 
 }
